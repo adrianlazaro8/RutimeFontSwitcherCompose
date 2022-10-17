c4192f7dd4cbf56203b99b6c506753623a09fbf1
Adrián Lázaro <adri92css@gmail.com>
2022-10-17 19:32:55 +0200

    Added Text for MainScreen

--- app/src/main/java/com/adlagar8/runtimefontswitcher/ui/screens/FontChooserScreen.kt
+++ app/src/main/java/com/adlagar8/runtimefontswitcher/ui/screens/FontChooserScreen.kt
@@ -1,8 +1,11 @@
 package com.adlagar8.runtimefontswitcher.ui.screens
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun FontChooserScreen(){
 
+    FontFinder()
+    Text("Escribe lo que quieras")
 }
