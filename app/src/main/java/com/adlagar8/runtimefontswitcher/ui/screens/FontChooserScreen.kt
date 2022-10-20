ffffa9c00e70aca1d8a74f0eee30719b4f302cb7
Adrián Lázaro <adri92css@gmail.com>
2022-10-20 18:24:38 +0200

    feat(font finder): string resource

--- app/src/main/java/com/adlagar8/runtimefontswitcher/ui/screens/FontChooserScreen.kt
+++ app/src/main/java/com/adlagar8/runtimefontswitcher/ui/screens/FontChooserScreen.kt
@@ -2,10 +2,12 @@ package com.adlagar8.runtimefontswitcher.ui.screens
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.adlagar8.runtimefontswitcher.R
 
 @Composable
 fun FontChooserScreen(){
 
     FontFinder()
-    Text("Escribe lo que quieras")
+    Text(stringResource(R.string.write_something))
 }
