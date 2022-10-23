049a178c310107790bc4dda37d26e5fdad26a05e
Adrián Lázaro <adri92css@gmail.com>
2022-10-23 10:40:52 +0200

    feat(provider): choosing font from textfield

--- app/src/main/java/com/adlagar8/runtimefontswitcher/ui/screens/FontFinder.kt
+++ app/src/main/java/com/adlagar8/runtimefontswitcher/ui/screens/FontFinder.kt
@@ -1,8 +1,27 @@
 package com.adlagar8.runtimefontswitcher.ui.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun FontFinder(){
+fun FontFinder(onTextChanged: (String) -> Unit) {
+
+    var currentText by remember { mutableStateOf("Escribe el texto que quieras") }
+
+    TextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        value = currentText,
+        onValueChange = {
+            currentText = it
+            if (currentText.isNotEmpty()) {
+                onTextChanged.invoke(it)
+            }
+        })
 
 }
