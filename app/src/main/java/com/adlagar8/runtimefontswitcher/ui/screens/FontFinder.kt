21202b9eaa87c45bde9ef2e0532bda992ba2ec49
Adrián Lázaro <adri92css@gmail.com>
2022-11-22 20:18:30 +0100

    feat(ui): placeholder added

--- app/src/main/java/com/adlagar8/runtimefontswitcher/ui/screens/FontFinder.kt
+++ app/src/main/java/com/adlagar8/runtimefontswitcher/ui/screens/FontFinder.kt
@@ -2,6 +2,7 @@ package com.adlagar8.runtimefontswitcher.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -10,13 +11,14 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun FontFinder(onTextChanged: (String) -> Unit) {
 
-    var currentText by remember { mutableStateOf("Escribe el texto que quieras") }
+    var currentText by remember { mutableStateOf("") }
 
     TextField(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp),
         value = currentText,
+        placeholder = { Text("Escribe el nombre de una fuente") },
         onValueChange = {
             currentText = it
             if (currentText.isNotEmpty()) {
