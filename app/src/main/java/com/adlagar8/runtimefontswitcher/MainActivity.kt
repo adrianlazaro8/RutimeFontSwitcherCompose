0156d7d0bf11ebab5bb3a3d05df954c88ba3253a
Adrián Lázaro <adri92css@gmail.com>
2022-12-29 21:46:24 +0100

    feat(preview): Previews removed

--- app/src/main/java/com/adlagar8/runtimefontswitcher/MainActivity.kt
+++ app/src/main/java/com/adlagar8/runtimefontswitcher/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.adlagar8.runtimefontswitcher.ui.screens.FontChooserScreen
 import com.adlagar8.runtimefontswitcher.ui.theme.MyApplicationTheme
 
@@ -28,18 +25,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
